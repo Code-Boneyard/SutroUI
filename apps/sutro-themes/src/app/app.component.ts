e6647829a22e015996c21624fc7b736b22e6ac60
Max Geller <mxgeller@gmail.com>
2020-09-07 22:41:10 -0700

    adding additional theme and analytics applications

---
+++ apps/sutro-themes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sutro-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sutro-themes';
+}
